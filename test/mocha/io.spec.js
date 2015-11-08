2686d2cbd1fcebdce996e1831e319e58927ae9d4
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-11-08 23:44:06 +0900

    update io.spec

--- test/mocha/io.spec.js
+++ test/mocha/io.spec.js
@@ -4,13 +4,34 @@ var expect = require('expect.js');
 var sys = require('sys');
 var fs = require('fs');
 
-// describe('IO', function() {
-//   describe('file', function() {
-//     it('read', function() {
-//       fs.readFileSync(path, 'utf8');
-//       expect(
-// 		io.file.read(path).length
-// 	  ).to.be(2391);
-//     });
-//   });
-// });
+describe('IO', function() {
+  describe('file', function() {
+	after(() => {
+	  fs.writeFileSync('test/resources/file.txt', "This is a test.");
+	});
+    it('read', (next) => {
+	  var file = fs.readFileSync("test/resources/file.txt", 'utf8');
+      expect(
+		file.length
+	  ).to.be(
+		15
+	  );
+      expect(
+		file
+	  ).to.be(
+		"This is a test."
+	  );
+	  next();
+    });
+    it('write', (next) => {
+	  fs.writeFileSync('test/resources/file.txt', "This is another test.")
+	  var file = fs.readFileSync("test/resources/file.txt", 'utf8');
+      expect(
+		file
+	  ).not.to.be(
+		"This is a test."
+	  );
+	  next();
+    });
+  });
+});
