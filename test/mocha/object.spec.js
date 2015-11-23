30d4869c2385324b63add52b734f0dd34135623b
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2015-11-24 07:42:35 +0900

    small fix

--- test/mocha/object.spec.js
+++ test/mocha/object.spec.js
@@ -46,6 +46,36 @@ describe("オブジェクト", () => {
 	  );
 	  next();
 	});
+	it("オブジェクトが空かどうかをチェックする", (next) => {
+      var hasOwnProperty = Object.prototype.hasOwnProperty;
+      var isEmpty = (obj) => {
+        // null and undefined are "empty"
+        if (obj == null) return true;
+        // Assume if it has a length property with a non-zero value
+        // that that property is correct.
+        if (obj.length > 0)    return false;
+        if (obj.length === 0)  return true;
+        // Otherwise, does it have any properties of its own?
+        // Note that this doesn't handle
+        // toString and valueOf enumeration bugs in IE < 9
+        for (var key in obj) {
+          if (hasOwnProperty.call(obj, key)) return false;
+        }
+        return true;
+      };
+	  expect(
+        isEmpty({ key: 1})
+      ).to.eql(
+        false
+      );
+	  expect(
+        isEmpty({})
+      ).to.eql(
+        true
+      );
+      
+      next();
+    });
   });
   describe('OOP related patterns', function() {
 	it("object literal pattern", function(next) {
