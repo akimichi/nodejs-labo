e8e94ab4d2a13e40cc15c63484372f2774886965
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-06-02 12:29:31 +0900

    update package

--- test/mocha/promise.spec.js
+++ test/mocha/promise.spec.js
@@ -6,7 +6,7 @@ describe("Promise", () => {
   describe("bluebird", () => {
     const Promise = require("bluebird");
 
-    it("test async function by promise",  (done) => {  
+    it("Promiseを使った関数を定義する",  (done) => {  
       // 非同期関数をpromiseで定義する
       let asyncFunction = () => {
         return new Promise((resolve, reject) => {
@@ -16,14 +16,16 @@ describe("Promise", () => {
         });
       };
       // 非同期関数をテストする
-      asyncFunction().then((value) => {
-        // expect(value).to.equal("")
-        expect(value).to.equal("Async Hello world")
-        done();
-      }).catch((error) => {
-        expect().fail(); 
-        done(error); 
-      });
+      asyncFunction()
+        .then((value) => {
+          // expect(value).to.equal("")
+          expect(value).to.equal("Async Hello world")
+          done();
+        })
+        .catch((error) => {
+          expect().fail(); 
+          done(error); 
+        });
     });
   });
 });
