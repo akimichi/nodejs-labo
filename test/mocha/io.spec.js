a4cbdd50a3deaa969d42fdaea77e5aebda861b3f
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2017-07-12 07:18:04 +0900

    fix layout

--- test/mocha/io.spec.js
+++ test/mocha/io.spec.js
@@ -6,49 +6,49 @@ var fs = require('fs');
 
 describe('IO', function() {
   describe('file', function() {
-	after(() => {
-	  fs.writeFileSync('test/resources/file.txt', "This is a test.");
-	});
+    after(() => {
+      fs.writeFileSync('test/resources/file.txt', "This is a test.");
+    });
     describe('read', () => {
       it('文字列として読み込む', (next) => {
-	    var file = fs.readFileSync("test/resources/file.txt", 'utf8');
+        var file = fs.readFileSync("test/resources/file.txt", 'utf8');
         expect(
-		  file.length
-	    ).to.be(
-		  15
-	    );
+          file.length
+        ).to.be(
+          15
+        );
         expect(
-		  file
-	    ).to.be(
-		  "This is a test."
-	    );
-	    next();
+          file
+        ).to.be(
+          "This is a test."
+        );
+        next();
       });
       it('JSONとして読み込む', (next) => {
         expect(
           JSON.parse(fs.readFileSync("test/resources/json.txt", 'utf8'))
-	    ).to.eql(
-		  {
+        ).to.eql(
+          {
             "a": "a string",
             "number": 2,
             "child": {
               "memo": "child node"
             }
           }
-	    );
-	    next();
+        );
+        next();
       });
     });
     describe('write', () => {
       it('文字列を出力する', (next) => {
-	    fs.writeFileSync('test/resources/file.txt', "This is another test.");
-	    var file = fs.readFileSync("test/resources/file.txt", 'utf8');
+        fs.writeFileSync('test/resources/file.txt', "This is another test.");
+        var file = fs.readFileSync("test/resources/file.txt", 'utf8');
         expect(
-		  file
-	    ).not.to.be(
-		  "This is a test."
-	    );
-	    next();
+          file
+        ).not.to.be(
+          "This is a test."
+        );
+        next();
       });
       it('JSONを出力する', (next) => {
         var object = {
@@ -58,13 +58,13 @@ describe('IO', function() {
             "memo": "child node"
           }
         };
-	    fs.writeFileSync('test/resources/json.txt',  JSON.stringify(object, null, '    '));
+        fs.writeFileSync('test/resources/json.txt',  JSON.stringify(object, null, '    '));
         expect(
           JSON.parse(fs.readFileSync("test/resources/json.txt", 'utf8'))
-	    ).to.eql(
-		  object
-	    );
-	    next();
+        ).to.eql(
+          object
+        );
+        next();
       });
     });
   });
